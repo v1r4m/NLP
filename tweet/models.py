c7570f65cfd2538a07fa46cbba5349440a3e0b9e
v1r4m <choyoung7789@gmail.com>
2022-09-28 18:03:06 +0900

    seems like everything's fine, but I think I have to migrate first.

--- tweet/models.py
+++ tweet/models.py
@@ -2,15 +2,13 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
-    pkWord = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    pkWord = models.CharField(max_length=10)
     pkLink = models.TextField()
+    join_word = models.CharField(max_length=10)
+    join_value = models.IntegerField()
     #pk = models.ForeignKey('tweet.Link') #####
     #pkWord = models.CharField(max_length=10)
-    join_words = models.ManyToManyField(settings.AUTH_USER_MODEL,
-                        verbose_name=u'relate', blank=True,
-                        related_name='join_word')
 
     def publish(self):
         self.published_date = timezone.now()
@@ -18,3 +16,4 @@ class Post(models.Model):
 
     def __str__(self):
         return self.pkLink
+
