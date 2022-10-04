723b2dca0820369fc75a768ec103b1e86587cc02
v1r4m <choyoung7789@gmail.com>
2022-10-04 11:04:53 +0900

    scheme control

--- tweet/models.py
+++ tweet/models.py
@@ -7,13 +7,8 @@ class Post(models.Model):
     pkWord = models.CharField(max_length=10)
     join_word = models.CharField(max_length=10)
     join_v = models.IntegerField()
-    published_date = models.DateTimeField(blank=True, null=True)
     #pk = models.ForeignKey('tweet.Link') #####
     #pkWord = models.CharField(max_length=10)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
 
     def __str__(self):
         return self.pkWord
