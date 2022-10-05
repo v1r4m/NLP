5640a27c016d82e414d560b7642fa78dfbe7afcb
v1r4m <choyoung7789@gmail.com>
2022-10-05 13:43:08 +0900

    force graph and miserable

--- nlp/src/App.js
+++ nlp/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import styled from "styled-components";
+import { CssBaseline } from "@mui/material";
+import ForceGraph from "./ForceGraph";
 
-function App() {
+const Main = styled("main")`
+  display: flex;
+  height: 100vh;
+`;
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      <CssBaseline />
+      {/*       <Sidebar>
+        <ToolBox />
+        <EditorBox
+          defaultValue={JSON.stringify(miserables.nodes, null, 2)}
+          title="Nodes"
+        />
+        <EditorBox
+          defaultValue={JSON.stringify(miserables.links, null, 2)}
+          title="Links"
+        />
+      </Sidebar> */}
+
+      <ForceGraph />
+
+      {/*       <GraphItem>
+        <ForceGraph />
+      </GraphItem> */}
+    </Main>
   );
 }
-
-export default App;
---
+++ nlp/src/ForceGraph.js
@@ -0,0 +1,150 @@
+import { useD3 } from "./useD3";
+import React from "react";
+import * as d3 from "d3";
+import data from "./miserables";
+
+function clamp(x, lo, hi) {
+  return x < lo ? lo : x > hi ? hi : x;
+}
+
+const ForceGraph = () => {
+  const width = 640; // outer width, in pixels
+  const height = 400; // outer height, in pixels
+  let nodes = data.nodes;
+  let links = data.links;
+
+  const ref = useD3((svg) => {
+    const nodeId = (d) => d.id;
+    const linkSource = ({ source }) => source; // given d in links, returns a node identifier string
+    const linkTarget = ({ target }) => target; // given d in links, returns a node identifier string
+    const N = d3.map(nodes, nodeId).map(intern);
+    const LS = d3.map(links, linkSource).map(intern);
+    const LT = d3.map(links, linkTarget).map(intern);
+    const nodeGroup = (d) => d.group;
+    const G = nodeGroup == null ? null : d3.map(nodes, nodeGroup).map(intern);
+    const colors = d3.schemeTableau10; // an array of color strings, for the node groups
+
+    // Replace the input nodes and links with mutable objects for the simulation.
+    nodes = d3.map(data.nodes, (_, i) => ({ id: N[i] }));
+    links = d3.map(data.links, (_, i) => ({
+      source: LS[i],
+      target: LT[i]
+    }));
+
+    console.log("links: ", links);
+    console.log("nodes: ", nodes);
+
+    svg
+      .attr("viewBox", [0, 0, width, height])
+      .attr("width", width)
+      .attr("height", height)
+      .attr("style", "background-color: lightgray");
+
+    const link = svg
+      .selectAll(".link")
+      .data(links)
+      .join("line")
+      .classed("link", true);
+
+    const node = svg
+      .selectAll(".node")
+      .data(nodes)
+      .join("circle")
+      .attr("r", 12)
+      .classed("node", true)
+      .classed("fixed", (d) => d.fx !== undefined);
+
+    svg.node();
+
+    /*     const simulation = d3
+      .forceSimulation()
+      .nodes(nodes)
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(width / 2, height / 2))
+      //.force("link", d3.forceLink(links))
+      .force("link", d3.forceLink(links))
+      .on("tick", tick); */
+
+    // Construct the scales.
+    let nodeGroups = d3.sort(G);
+    console.log(nodeGroups);
+    const color =
+      nodeGroup == null ? null : d3.scaleOrdinal(nodeGroups, colors);
+
+    // Construct the forces.
+    const forceNode = d3.forceManyBody();
+    const forceLink = d3.forceLink(links).id(({ index: i }) => N[i]);
+
+    const simulation = d3
+      .forceSimulation(nodes)
+      .force("link", forceLink)
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(width / 2, height / 2))
+      .on("tick", tick);
+
+    if (G)
+      node.attr("fill", ({ index: i }) => {
+        console.log("index: ", i);
+        return color(G[i]);
+      });
+
+    const drag = d3
+      .drag()
+      .on("start", dragstart)
+      .on("drag", dragged)
+      .on("end", dragend);
+
+    node.call(drag);
+
+    function tick() {
+      link
+        .attr("x1", (d) => d.source.x)
+        .attr("y1", (d) => d.source.y)
+        .attr("x2", (d) => d.target.x)
+        .attr("y2", (d) => d.target.y);
+      node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+    }
+
+    function intern(value) {
+      return value !== null && typeof value === "object"
+        ? value.valueOf()
+        : value;
+    }
+
+    function dragstart() {
+      d3.select(this).classed("fixed", true);
+    }
+
+    function dragend(event, d) {
+      if (event.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
+
+    function dragged(event, d) {
+      d.fx = clamp(event.x, 0, width);
+      d.fy = clamp(event.y, 0, height);
+      simulation.alpha(1).restart();
+    }
+  });
+
+  return (
+    <svg
+      ref={ref}
+      style={{
+        marginRight: "0px",
+        marginLeft: "0px",
+        backgroundColor: "green"
+      }}
+    >
+      <text x="5" y="20">
+        d3-react-force-graph-les-miserables
+      </text>
+      <text x="5" y="40">
+        {`${width}x${height}`}
+      </text>
+    </svg>
+  );
+};
+
+export default ForceGraph;
